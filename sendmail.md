d81607814b351252627a7e5f168e715e3975ce60
David Flenner <30780186+davidf628@users.noreply.github.com>
2024-10-06 12:43:14 -0400

    added sendmail
    Added my sendmail function to this library to make it easier to find and standardize the use of this function

---
+++ sendmail.md
@@ -0,0 +1,12 @@
+### Sendmail
+
+This is a python module designed to make it easy to send a message through email in an automated fashion. To use this you must have a gmail username and password combination that is sent to the function for logging in - and the security protocols must be reduced in order for them to work, so I wouldn't advise using your main login.
+
+You can set an alternate reply-to address to change where replies are sent to.
+
+Version History:
+
+2.1.0 - 10/6/2024
+  I edited the library to share from GitHub so that I could more easily find it and use it when necessary.
+
+2.0.0 - 12/16/2021
---
+++ sendmail.py
@@ -0,0 +1,57 @@
+
+VERSION = "2.1.0"
+LAST_UPDATE = "10/6/2025"
+
+import smtplib
+from email.message import EmailMessage
+import mimetypes
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
+
+###############################################################################
+## Sends an email using a gmail account, but the account must be set up 
+##
+## Parameters:
+##   - user (string): the username of the gmail account to send from
+##   - password (string): the password of the gmail account
+##   - sendto (string): the email address to send the message to
+##   - body (string): the text of the message
+##   - subject (string): the subject title
+##   - args (dict):
+##       - reply-to (string): the default reply-to address if it is different
+##           from the sending address
+##       - attachment (string): the location of a file to attach to the message
+##
+
+def sendmail(user, password, sendto='', body='', subject='', args=None):
+
+    msg = EmailMessage()
+ 
+    msg['from'] = user
+    msg['to'] = sendto
+    msg['subject'] = subject
+    
+    msg.set_content(body)
+
+    if args != None:
+
+        if 'reply-to' in args.keys():
+            msg['reply-to'] = args['reply-to']
+
+        if 'attachment' in args.keys():
+            filename = os.path.basename(args['attachment'])
+            mime_type, encoding = mimetypes.guess_type(args['attachment'])
+            mime_type, mime_subtype = mime_type.split('/', 1)
+
+            with open(args['attachment'], 'rb') as file:
+                msg.add_attachment(file.read(), maintype=mime_type, subtype=mime_subtype, filename=filename)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+
+    server.quit()
