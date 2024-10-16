462beddac272649961aecd4da826f4ba8d5eee9d
davidf628 <30780186+davidf628@users.noreply.github.com>
2024-10-15 21:02:03 -0400

    added query
    added the ability to query data from a dictarray

--- dictarray.py
+++ dictarray.py
@@ -25,3 +25,25 @@ def format_col(col_name, func, data):
         record[col_name] = func(record[col_name])
     
     return data
+
+def query(data, /, select, where, equals):
+    """This will perform a simple query on a list of objects loaded into memory by 
+    finding the item 'select' which corresponds to the item 'where == value'
+    if the 'value' is not found, None is returned
+
+    Args:
+        data (object[]): the array of objects to search through
+        select (str): the value within the record that matched 
+        where (str): the field name to look through
+        value (str): the value to search for
+
+    Returns:
+        object or None: the object that matched the query, or None
+    """
+    for record in data:
+        if record[where] == equals:
+            if select == None:
+                return record
+            else:
+                return record[select]
+    return None
