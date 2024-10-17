3403a383e3b3610de1513ad40e40c762c7e02f00
davidf628 <30780186+davidf628@users.noreply.github.com>
2024-10-17 09:57:35 -0400

    update get_dependency
    updated get_dependency to stop execution if a dependency is not located online

--- init.py
+++ init.py
@@ -7,5 +7,7 @@ def get_dependency(script, path=''):
     website += script
     if not os.path.exists(script):
         response = requests.get(f'{website}{script}')
+        if response == '404: Not Found':
+            SystemExit(f'Dependency: {website} not found')
         with open(script, mode='wb') as file:
             file.write(response.content)
