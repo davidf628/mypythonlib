add8012ea6f963e10f259974e5e1dd133cd4df73
davidf628 <30780186+davidf628@users.noreply.github.com>
2024-12-17 17:31:40 -0500

    added find_last_row
    this function determines the last row that data actually exists within a column

--- excelfile.py
+++ excelfile.py
@@ -29,3 +29,22 @@ def load_dictarray(wbfile):
     wb.close()
             
     return dict_array
+
+###############################################################################
+# Grabbed from stack overflow to figure out a better estimate on the number of
+#  students in the worksheet since ws.max_row often gives an overestimate based
+#  on previously deleted data
+
+def find_last_row(ws, column):
+    """
+    Returns the last row of a given column that has a value in it.
+    Empty rows before the last row are allowed.
+    ws: a worksheet object
+    column: a name, such as 'A'
+    """
+    selected_column = ws[column]
+    # Start at bottom of column
+    for row in range(len(selected_column)-1, -1, -1):
+        if selected_column[row].value != None:
+            return row + 1 # +1 since Excel row numbering starts at 1
+    return None # Empty column
